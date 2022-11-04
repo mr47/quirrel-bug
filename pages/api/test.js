f7b32d9f0696a8b3dc63603d8e8c31cdd5f80ec6
ae_dpiddubnyi <ae_dpiddubnyi@idcontractors.net>
2022-11-04 22:23:52 +0200

    Updated cronjob exp

--- pages/api/test.js
+++ pages/api/test.js
@@ -2,7 +2,7 @@ import { CronJob } from "quirrel/next";
 
 export default CronJob(
     "api/test",
-    "*/1 * * * *",
+    "* * * * *",
     async () => {
         console.log("I am a cron job running every minute");
     }
